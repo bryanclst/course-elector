f6d511e8f891c7b4cc1939be165cf55953a00454
telehops <bryanfaryadi@gmail.com>
2023-11-01 20:00:38 -0400

    commented code from models because they will likely be represented with an SQL database. also renamed view_rating -> view_ratings

--- src/models/course.py
+++ src/models/course.py
@@ -1,5 +1,5 @@
-class Course:
-    def __init__(self, title: str, dept: str, code: int) -> None:
-        self.title = title
-        self.dept = dept
-        self.code = code
+# class Course:
+#     def __init__(self, title: str, dept: str, code: int) -> None:
+#         self.title = title
+#         self.dept = dept
+#         self.code = code
--- src/models/rating.py
+++ src/models/rating.py
@@ -1,10 +1,10 @@
-from src.models.course import Course
+# from src.models.course import Course
 
-class Rating:
-    def __init__(self, course: Course, prof: str, quality: int, difficulty: int, grade: str, desc: str) -> None:
-        self.course = course
-        self.prof = prof
-        self.quality = quality
-        self.difficulty = difficulty
-        self.grade = grade
-        self.desc = desc
+# class Rating:
+#     def __init__(self, course: Course, prof: str, quality: int, difficulty: int, grade: str, desc: str) -> None:
+#         self.course = course
+#         self.prof = prof
+#         self.quality = quality
+#         self.difficulty = difficulty
+#         self.grade = grade
+#         self.desc = desc
