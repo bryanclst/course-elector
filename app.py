5665ea1f97c29dfb05d9b226af59399001655de2
kiana <kianatruong03@gmail.com>
2023-11-05 00:05:41 -0400

    homepage and userprofile ui still finishing touches

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.route('/userprofile')
+def profile():
+    return render_template('userprofile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
