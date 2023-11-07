50cfe995fe531d5ed88d3073048fd47c936a1b98
telehops <bryanfaryadi@gmail.com>
2023-11-07 17:51:02 -0500

    added variables to dynamically pick which header to highlight

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def index():
 
 @app.get('/submit_rating')
 def get_rating_form():
-    return render_template('submit_rating.html')
+    return render_template('submit_rating.html', rating_active=True)
 
 @app.post('/submit_rating')
 def submit_rating():
-    return render_template('view_ratings.html')
+    return render_template('view_ratings.html', rating_active=True)
