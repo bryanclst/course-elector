a4cbda73d2304892197af95ae5e2a48ccd7f1aee
Nicholas_Zoll <nickzll@yahoo.com>
2023-11-08 05:33:11 -0500

    fixed conflict in app,py

--- app.py
+++ app.py
@@ -41,3 +41,11 @@ def course_directory():
 
     return render_template('course_directory.html', major_choice=major_choice, course_active=True) #would in theory be feeding in list of courses, just using major_choice for testing
 
+@app.get('/submit_rating')
+def get_rating_form():
+    return render_template('submit_rating.html', rating_active=True)
+
+@app.post('/submit_rating')
+def submit_rating():
+    return render_template('view_ratings.html', rating_active=True)
+
