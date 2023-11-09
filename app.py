1cf085ebac192e7cb68f0deb86ae759ca0fed977
kiana <kianatruong03@gmail.com>
2023-11-09 10:28:46 -0500

    mock ui for profile page

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('index.html')
 
-@app.route('/userprofile')
-def profile():
-    return render_template('userprofile.html')
-
 @app.route('/login_signup')
 def login_signup():
     return render_template('login_signup.html')
+
+@app.route('/userprofile')
+def userprofile():
+    return render_template('userprofile.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
