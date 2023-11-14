1a12aa078021f6a337c9decabcdb13e95c793770
telehops <bryanfaryadi@gmail.com>
2023-11-13 22:27:41 -0500

    finished UI for view rating page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -12,4 +12,9 @@ def get_rating_form():
 
 @app.post('/submit_rating')
 def submit_rating():
+    # do stuff with rating form
+    return redirect('/view_ratings') # TODO change to append the id of the rating eventually
+
+@app.get('/view_ratings') # TODO variable to access specific id
+def view_ratings():
     return render_template('view_ratings.html', rating_active=True)
