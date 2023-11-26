479b292ebc2c43dfc6e0d30e6d7157da5533834f
Nicholas_Zoll <nickzll@yahoo.com>
2023-11-26 03:46:40 -0500

    adding app.py logic

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 
 from src.repositories.repository import repository_singleton
 
+from sqlalchemy import func, cast, Float 
+from sqlalchemy.orm import joinedload 
 load_dotenv()
 
 app = Flask(__name__, static_url_path='/static')
@@ -125,23 +127,95 @@ def post_search():
     major_choice = request.form.get('major') #getting form data from course_directory_search.html
     quality_choice = request.form.get('quality') 
     difficulty_choice = request.form.get('difficulty') 
-    instructor_choice = request.form.get('instructorID')
+    credit_choice = request.form.get('credit')
 
-    query_parameters = f'major={major_choice}&quality={quality_choice}&difficulty={difficulty_choice}&instructorID={instructor_choice}' # query in string format, so that the information gained from the forms can be used in course_directory()
-    
-    return redirect(f'/directory?{query_parameters}') #redirects to /directory with the appropriate query parameters that contain the data from the forms 
+    # query in string format, so that the information gained from the forms can be used in course_directory()
+    query_parameters = f'major={major_choice}&quality={quality_choice}&difficulty={difficulty_choice}&credit={credit_choice}' 
+
+    #redirects to /directory with the appropriate query parameters that contain the data from the forms 
+    return redirect(f'/directory?{query_parameters}') 
 
 
 @app.get('/directory')
 def course_directory():
-    #Nick
-    major_choice = request.args.get('major') #getting the data from the url query parameter
-    quality_choice = request.args.get('quality') 
-    difficulty_choice = request.args.get('difficulty') 
-    instructor_choice = request.args.get('instructorID')
+    # get query parameters from the URL
+    major_choice = request.args.get('major')
+    quality_choice = request.args.get('quality')
+    difficulty_choice = request.args.get('difficulty')
+    credit_choice = request.args.get('credit')
 
-    #implement way to get list of courses based upon search parameters
+    # starting/base query, joinedload also loads related ratings, not neccessary but I think it helps reduce potential amount of queries made to the db
+    # these resourced helped me understand joinedload better: https://docs.sqlalchemy.org/en/20/orm/queryguide/relationships.html#sqlalchemy.orm.joinedload, https://stackoverflow.com/questions/56440296/how-does-the-sqlalchemy-loading-methods-work
+    courses_query = Course.query.options(joinedload(Course.ratings))
 
-    return render_template('course_directory.html', major_choice=major_choice, course_active=True) #would in theory be feeding in list of courses, just using major_choice for testing
+    # filter result if major parameter was selected
+    if major_choice:
+        courses_query = courses_query.filter_by(major=major_choice)
 
+    # filter result if credit hour parameter was selected
+    if credit_choice:
+        courses_query = courses_query.filter_by(credits=credit_choice)
 
+    # subquery, query nested inside another query, to get average difficulty and quality for each course using sqlalchemy function for averaging
+    # resources that helped me: https://blog.miguelgrinberg.com/post/nested-queries-with-sqlalchemy-orm, https://stackoverflow.com/questions/7143235/how-to-use-avg-and-sum-in-sqlalchemy-query, https://stackoverflow.com/questions/11830980/sqlalchemy-simple-example-of-sum-average-min-max
+    subquery = (
+        db.session.query(
+            Rating.course_id,
+            func.avg(Rating.difficulty).label('avg_difficulty'),
+            func.avg(Rating.quality).label('avg_quality')
+        )
+        .group_by(Rating.course_id)
+        .subquery()
+    )
+
+    # joining subquery with the base query on course_id
+    filtered_courses_query = courses_query.outerjoin(subquery, Course.course_id == subquery.c.course_id)  #.c for column
+
+    # filtered_courses initialization
+    filtered_courses = None
+
+    # filter result if course quality parameter was selected
+    if quality_choice:
+        if quality_choice == "Quality1Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_quality >= 1.0) #include only the courses where the average quality is 1.0 or higher using subquery column for average from earlier 
+        elif quality_choice == "Quality2Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_quality >= 2.0)
+        elif quality_choice == "Quality3Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_quality >= 3.0)
+        elif quality_choice == "Quality4Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_quality >= 4.0)
+        elif quality_choice == "Quality5Only":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_quality == 5.0)
+
+    # filter result if course difficulty parameter was selected
+    if difficulty_choice:
+        if difficulty_choice == "Diff1Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_difficulty >= 1.0) #include only the courses where the average difficulty is 1.0 or higher using subquery column from earlier 
+        elif difficulty_choice == "Diff2Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_difficulty >= 2.0)
+        elif difficulty_choice == "Diff3Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_difficulty >= 3.0)
+        elif difficulty_choice == "Diff4Plus":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_difficulty >= 4.0)
+        elif difficulty_choice == "Diff5Only":
+            filtered_courses_query = filtered_courses_query.filter(subquery.c.avg_difficulty == 5.0)       
+
+    #filtered query
+    filtered_courses = filtered_courses_query.all()
+
+    # loop for calculating average quality and difficulty for each course
+    for course in filtered_courses:
+        course.avg_quality = course.avg_difficulty = None
+        for rating in course.ratings: #loop through each rating for the course
+            if course.avg_quality is None:
+                course.avg_quality = 0
+            if course.avg_difficulty is None:
+                course.avg_difficulty = 0
+            course.avg_quality += rating.quality  #add up total for course
+            course.avg_difficulty += rating.difficulty
+        if course.avg_quality is not None:
+            course.avg_quality /= len(course.ratings) #calculate average by dividing by amount of ratings
+        if course.avg_difficulty is not None:
+            course.avg_difficulty /= len(course.ratings) #calculate average by dividing by amount of ratings
+
+    return render_template('course_directory.html', major_choice=major_choice, all_courses=filtered_courses, course_active=True)
