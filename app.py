d94c2f92fa87f050cb97c56e030375bd6b65d63e
kiana <kianatruong03@gmail.com>
2023-11-26 23:56:02 -0500

    looked over changes request and adjusted them to see if they actually work. Need to rework features later on if they cause issues. Git is having issues. :(

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ import os
 from src.repositories.repository import repository_singleton
 
 load_dotenv()
-bcrypt = Bcrypt(app)
-
 app = Flask(__name__, static_url_path='/static')
-
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+
+app.secret_key= os.getenv('APP_SECRET_KEY', 'abc')
+
+bcrypt = Bcrypt(app)
 db.init_app(app)
 
 #structures to hold posts and comments
@@ -105,25 +106,29 @@ def process_form():
         new_user = AppUser(username=username, hashed_password=bcrypt_password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect('/login_signup.html')
+        session['username'] = username
+        return redirect('/login_signup')
     elif action == 'Login':
-        username=request.form.get('username')
-        hashed_password=request.form.get('hashed_password')
+        existing_user = AppUser.query.filter_by(username=username).first_or_404()
         if not username or not hashed_password:
             abort (400)
-        existing_user = AppUser.query.filter_by(username=username).first_or_404()
         if not existing_user:
             abort(401)
-        if not bcrypt.check_password_hash(existing_user.password, hashed_password):
+        if not bcrypt.check_password_hash(existing_user.hashed_password, hashed_password):
             abort(401)
+        session['username'] = username
         return redirect('/user_profile')
     else:
         return 'Invalid action'
 
 @app.route('/user_profile')
 def userprofile():
-    if 'username' not in session:
-        abort(401)
+    username = session.get('username')
+    
+    if username is None:
+        # Redirect to login page or handle unauthorized access
+        return redirect('/login_signup')     
+    
     return render_template('user_profile.html', username=session['username'], user_active=True)
 
 @app.get('/submit_rating')
