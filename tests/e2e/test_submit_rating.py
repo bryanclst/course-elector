b6a042fcf94057203bfc9d200f3ed08ae272b187
telehops <bryanfaryadi@gmail.com>
2023-11-27 22:29:57 -0500

    asdf

--- tests/e2e/test_submit_rating.py
+++ tests/e2e/test_submit_rating.py
@@ -1,6 +1,6 @@
 from src.models import AppUser, Course, Rating, Post, Comment
-from utils import clear_db, populate_db
+from utils import clear_db, populate_db, heavily_populate_db
 
 def test(test_client):
-    populate_db()
+    heavily_populate_db()
     assert True
